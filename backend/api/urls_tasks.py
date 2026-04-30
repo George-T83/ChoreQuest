995cffb32d5d85a34695ae8187ba30cf3102316f
George T <1fungenius@gmail.com>
2026-04-29 21:15:53 -0500

    Adds update task API and endpoint

--- backend/api/urls_tasks.py
+++ backend/api/urls_tasks.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('create/', views_tasks.create_task),
     path('household/', views_tasks.get_household_tasks),
     path('<str:task_id>/complete/', views_tasks.complete_task),
+    path('<str:task_id>/update/', views_tasks.update_task),
 ]
